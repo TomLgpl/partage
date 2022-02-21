5d80665eb0b6583d54ef215d7d3cf0ae6ba3d32a
TomLgpl <tomlegoupilgmail.com>
2022-02-21 21:24:57 +0100

    update accueil

--- templates/Pages/index.php
+++ templates/Pages/index.php
@@ -3,8 +3,14 @@
 ?>
 
 <div class="ui container">
-    <div class="ui inverted segment">
-        <h1 class="ui header"><?= __('Bientôt dispo') ?></h1>
+    <div class="ui segment">
+        <h1 class="ui header"><?= __('En cours de développement') ?></h1>
+        <br>
+        Pour voir tes photos, il suffit d'aller dans l'onglet <b>Mes photos</b>.
+        <br>
+        En cas de soucis, n'hésites pas à me contacter !
+        <br>
+        <a href="mailto:tomlegoupil@gmail.com">tomlegoupil@gmail.com</a>
     </div>
 </div>
 
